4271329a54c3752d941ea9ddac55fd076a8fcacf
kibukj <kibuzhj@gmail.com>
2020-10-13 18:14:55 -0400

    Move config options to config.lua

--- client.lua
+++ client.lua
@@ -1,48 +1,7 @@
--- === CONFIGURATION ===
-
--- Configurable controls
-local IncreaseSpeedControl = 0x446258B6 -- Page Up
-local DecreaseSpeedControl = 0x3C3DD371 -- Page Down
-local UpControl            = 0xD9D0E1C0 -- Spacebar
-local DownControl          = 0x8FFC75D6 -- Shift
-local ForwardControl       = 0x8FD015D8 -- W
-local BackwardControl      = 0xD27782E3 -- S
-local LeftControl          = 0x7065027D -- A
-local RightControl         = 0xB4E465B4 -- D
-local IncreaseFovControl   = 0x9959A6F0 -- C
-local DecreaseFovControl   = 0x8CC9CD42 -- X
-local RollLeftControl      = 0x26E9DC00 -- Z
-local RollRightControl     = 0x7F8D09B8 -- V
-local ToggleHud            = 0xDE794E3E -- Q
-local ResetCamControl      = 0x4CC0E2FE -- B
-
--- Camera movement speed
-local MaxSpeed = 10.0
-local MinSpeed = 0.1
-local SpeedIncrement = 0.1
-
--- Default speed
-local Speed = 0.1
-
--- Camera zoom/FOV
-local MaxFov = 120.0
-local MinFov = 20.0
-local ZoomSpeed = 1.0
-
--- Camera rotation speed
-local SpeedLr = 8.0
-local SpeedUd = 8.0
-
--- Camera roll speed
-local RollSpeed = 1.0
-
--- Whether to show/hide the freecam controls and HUD by default
-local ShowHud = true
-
--- === END OF CONFIGURATION ===
-
 local Cam = nil
 local StartingFov = 0.0
+local ShowHud = true
+local Speed = Config.Speed
 
 function EnableFreeCam()
 	FreezeEntityPosition(PlayerPedId(), true)
@@ -103,58 +62,58 @@ CreateThread(function()
 			local fov = GetCamFov(Cam)
 
 			-- Ensure speed is within the specified range
-			if Speed < MinSpeed then
-				Speed = MinSpeed
+			if Speed < Config.MinSpeed then
+				Speed = Config.MinSpeed
 			end
-			if Speed > MaxSpeed then
-				Speed = MaxSpeed
+			if Speed > Config.MaxSpeed then
+				Speed = Config.MaxSpeed
 			end
 
 			-- Ensure FOV is within the specified range
-			if fov < MinFov then
-				fov = MinFov
+			if fov < Config.MinFov then
+				fov = Config.MinFov
 			end
-			if fov > MaxFov then
-				fov = MaxFov
+			if fov > Config.MaxFov then
+				fov = Config.MaxFov
 			end
 
 			-- Show controls or hide HUD
 			if ShowHud then
 				DrawText(string.format('FreeCam Speed: %.1f', Speed), 0.5, 0.90, true)
 				DrawText(string.format('Coordinates:\nX: %.2f\nY: %.2f\nZ: %.2f\nPitch: %.2f\nRoll: %.2f\nYaw: %.2f\nFOV: %.0f', x, y, z, pitch, roll, yaw, fov), 0.01, 0.3, false)
-				DrawText('W/A/S/D - Move, Spacebar/Shift - Up/Down, Page Up/Page Down - Change speed, C/X - Zoom, Z/V - Roll, B - Reset, Q - Hide HUD', 0.5, 0.95, true)
+				DrawText('W/A/S/D - Move, Spacebar/Shift - Up/Down, Page Up/Page Down - Change speed, Z/X - Zoom, C/V - Roll, B - Reset, Q - Hide HUD', 0.5, 0.95, true)
 			else
 				HideHudAndRadarThisFrame()
 			end
 
 			-- Toggle HUD
-			if IsControlJustPressed(0, ToggleHud) then
+			if IsControlJustPressed(0, Config.ToggleHudControl) then
 				ShowHud = not ShowHud
 			end
 
 			-- Reset camera
-			if IsControlJustPressed(0, ResetCamControl) then
+			if IsControlJustPressed(0, Config.ResetCamControl) then
 				SetCamRot(Cam, 0.0, 0.0, 0.0, 2)
 				SetCamFov(Cam, StartingFov)
 			end
 
 			-- Increase movement speed
-			if IsControlPressed(0, IncreaseSpeedControl) then
-				Speed = Speed + SpeedIncrement
+			if IsControlPressed(0, Config.IncreaseSpeedControl) then
+				Speed = Speed + Config.SpeedIncrement
 			end
 
 			-- Decrease movement speed
-			if IsControlPressed(0, DecreaseSpeedControl) then
-				Speed = Speed - SpeedIncrement
+			if IsControlPressed(0, Config.DecreaseSpeedControl) then
+				Speed = Speed - Config.SpeedIncrement
 			end
 
 			-- Move up
-			if IsControlPressed(0, UpControl) then
+			if IsControlPressed(0, Config.UpControl) then
 				SetCamCoord(Cam, x, y, z + Speed)
 			end
 
 			-- Move down
-			if IsControlPressed(0, DownControl) then
+			if IsControlPressed(0, Config.DownControl) then
 				SetCamCoord(Cam, x, y, z - Speed)
 			end
 
@@ -163,19 +122,19 @@ CreateThread(function()
 			local axisY = GetDisabledControlNormal(0, 0xD2047988)
 
 			if axisX ~= 0.0 or axisY ~= 0.0 then
-				local newZ = yaw + axisX * -1.0 * SpeedUd * 1.0
-				local newX = math.max(math.min(89.9, pitch + axisY * -1.0 * SpeedLr * 1.0), -89.9)
+				local newZ = yaw + axisX * -1.0 * Config.SpeedUd * 1.0
+				local newX = math.max(math.min(89.9, pitch + axisY * -1.0 * Config.SpeedLr * 1.0), -89.9)
 				SetCamRot(Cam, newX, roll, newZ, 2)
 			end
 
 			-- Roll left
-			if IsControlPressed(0, RollLeftControl) then
-				SetCamRot(Cam, pitch, roll - RollSpeed, yaw, 2)
+			if IsControlPressed(0, Config.RollLeftControl) then
+				SetCamRot(Cam, pitch, roll - Config.RollSpeed, yaw, 2)
 			end
 
 			-- Roll right
-			if IsControlPressed(0, RollRightControl) then
-				SetCamRot(Cam, pitch, roll + RollSpeed, yaw, 2)
+			if IsControlPressed(0, Config.RollRightControl) then
+				SetCamRot(Cam, pitch, roll + Config.RollSpeed, yaw, 2)
 			end
 
 			-- Determine change in forward/backward movement
@@ -189,33 +148,33 @@ CreateThread(function()
 			local dy2 = Speed * math.cos(r2)
 
 			-- Move forward
-			if IsControlPressed(0, ForwardControl) then
+			if IsControlPressed(0, Config.ForwardControl) then
 				SetCamCoord(Cam, x + dx1, y + dy1, z)
 			end
 
 			-- Move backward
-			if IsControlPressed(0, BackwardControl) then
+			if IsControlPressed(0, Config.BackwardControl) then
 				SetCamCoord(Cam, x - dx1, y - dy1, z)
 			end
 
 			-- Move left
-			if IsControlPressed(0, LeftControl) then
+			if IsControlPressed(0, Config.LeftControl) then
 				SetCamCoord(Cam, x + dx2, y + dy2, z)
 			end
 
 			-- Move right
-			if IsControlPressed(0, RightControl) then
+			if IsControlPressed(0, Config.RightControl) then
 				SetCamCoord(Cam, x - dx2, y - dy2, z)
 			end
 
 			-- Increase FOV
-			if IsControlPressed(0, IncreaseFovControl) then
-				SetCamFov(Cam, fov + ZoomSpeed)
+			if IsControlPressed(0, Config.IncreaseFovControl) then
+				SetCamFov(Cam, fov + Config.ZoomSpeed)
 			end
 
 			-- Decrease FOV
-			if IsControlPressed(0, DecreaseFovControl) then
-				SetCamFov(Cam, fov - ZoomSpeed)
+			if IsControlPressed(0, Config.DecreaseFovControl) then
+				SetCamFov(Cam, fov - Config.ZoomSpeed)
 			end
 		end
 	end
---
+++ config.lua
@@ -0,0 +1,47 @@
+Config = {}
+
+-- Configurable controls
+Config.IncreaseSpeedControl = 0x446258B6 -- Page Up
+Config.DecreaseSpeedControl = 0x3C3DD371 -- Page Down
+Config.UpControl            = 0xD9D0E1C0 -- Spacebar
+Config.DownControl          = 0x8FFC75D6 -- Shift
+Config.ForwardControl       = 0x8FD015D8 -- W
+Config.BackwardControl      = 0xD27782E3 -- S
+Config.LeftControl          = 0x7065027D -- A
+Config.RightControl         = 0xB4E465B4 -- D
+Config.IncreaseFovControl   = 0x8CC9CD42 -- C
+Config.DecreaseFovControl   = 0x26E9DC00 -- X
+Config.RollLeftControl      = 0x9959A6F0 -- Z
+Config.RollRightControl     = 0x7F8D09B8 -- V
+Config.ToggleHudControl     = 0xDE794E3E -- Q
+Config.ResetCamControl      = 0x4CC0E2FE -- B
+
+-- Maximum movement speed
+Config.MaxSpeed = 10.0
+
+-- Minimum movement speed
+Config.MinSpeed = 0.1
+
+-- How much the speed increases/decreases by when the speed up/down controls are pressed
+Config.SpeedIncrement = 0.1
+
+-- Default movement speed
+Config.Speed = 0.1
+
+-- Maximum FOV
+Config.MaxFov = 120.0
+
+-- Minimum FOV
+Config.MinFov = 20.0
+
+-- How much the FOV increases/decreases by
+Config.ZoomSpeed = 1.0
+
+-- Camera rotation X-axis speed
+Config.SpeedLr = 8.0
+
+-- Camera rotation Y-axis speed
+Config.SpeedUd = 8.0
+
+-- Camera roll speed
+Config.RollSpeed = 1.0

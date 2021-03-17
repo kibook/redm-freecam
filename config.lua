32d380e3728cbfb5c6840fad2605f5ddff25edfd
kibukj <kibuzhj@gmail.com>
2021-03-17 15:40:47 -0400

    Change default controls

--- config.lua
+++ config.lua
@@ -1,24 +1,24 @@
 Config = {}
 
 -- Configurable controls
-Config.IncreaseSpeedControl = {`INPUT_CREATOR_LT`, `INPUT_PREV_WEAPON`} -- Page Up, Mouse Wheel Up
-Config.DecreaseSpeedControl = {`INPUT_CREATOR_RT`, `INPUT_NEXT_WEAPON`} -- Page Down, Mouse Wheel Down
-Config.UpControl            = `INPUT_JUMP`                              -- Spacebar
-Config.DownControl          = `INPUT_SPRINT`                            -- Shift
-Config.ForwardControl       = `INPUT_MOVE_UP_ONLY`                      -- W
-Config.BackwardControl      = `INPUT_MOVE_DOWN_ONLY`                    -- S
-Config.LeftControl          = `INPUT_MOVE_LEFT_ONLY`                    -- A
-Config.RightControl         = `INPUT_MOVE_RIGHT_ONLY`                   -- D
-Config.IncreaseFovControl   = `INPUT_GAME_MENU_TAB_RIGHT_SECONDARY`     -- Z
-Config.DecreaseFovControl   = `INPUT_GAME_MENU_TAB_LEFT_SECONDARY`      -- X
-Config.RollLeftControl      = `INPUT_LOOK_BEHIND`                       -- C
-Config.RollRightControl     = `INPUT_NEXT_CAMERA`                       -- V
-Config.ToggleHudControl     = `INPUT_COVER`                             -- Q
-Config.ResetCamControl      = `INPUT_OPEN_SATCHEL_MENU`                 -- B
-Config.PrevFilterControl    = `INPUT_CONTEXT_B`                         -- F
-Config.NextFilterControl    = `INPUT_INTERACT_OPTION1`                  -- G
-Config.ToggleFilterControl  = `INPUT_WHISTLE`                           -- H
-Config.ToggleGridControl    = `INPUT_OPEN_JOURNAL`                      -- J
+Config.IncreaseSpeedControl = {`INPUT_CREATOR_LT`, `INPUT_PREV_WEAPON`}    -- Page Up, Mouse Wheel Up
+Config.DecreaseSpeedControl = {`INPUT_CREATOR_RT`, `INPUT_NEXT_WEAPON`}    -- Page Down, Mouse Wheel Down
+Config.UpControl            = `INPUT_JUMP`                                 -- Spacebar
+Config.DownControl          = `INPUT_SPRINT`                               -- Shift
+Config.ForwardControl       = `INPUT_MOVE_UP_ONLY`                         -- W
+Config.BackwardControl      = `INPUT_MOVE_DOWN_ONLY`                       -- S
+Config.LeftControl          = `INPUT_MOVE_LEFT_ONLY`                       -- A
+Config.RightControl         = `INPUT_MOVE_RIGHT_ONLY`                      -- D
+Config.IncreaseFovControl   = `INPUT_GAME_MENU_TAB_RIGHT_SECONDARY`        -- Z
+Config.DecreaseFovControl   = `INPUT_GAME_MENU_TAB_LEFT_SECONDARY`         -- X
+Config.RollLeftControl      = `INPUT_LOOK_BEHIND`                          -- C
+Config.RollRightControl     = `INPUT_NEXT_CAMERA`                          -- V
+Config.ToggleHudControl     = `INPUT_COVER`                                -- Q
+Config.ResetCamControl      = `INPUT_OPEN_SATCHEL_MENU`                    -- B
+Config.PrevFilterControl    = `INPUT_CONTEXT_B`                            -- F
+Config.NextFilterControl    = `INPUT_INTERACT_ANIMAL`                      -- G
+Config.ToggleFilterControl  = {`INPUT_WHISTLE`, `INPUT_WHISTLE_HORSEBACK`} -- H
+Config.ToggleGridControl    = `INPUT_OPEN_JOURNAL`                         -- J
 
 -- Maximum movement speed
 Config.MaxSpeed = 1.00
